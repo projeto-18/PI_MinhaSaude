a195c6309e9fc5c8bb97cf8a79f425be065dc04c
@fcamposdevoficial <113559299+Fcamposdev@users.noreply.github.com>
2023-09-06 14:39:09 -0300

    Tela Sentimentos
    a tela de sentimentos com nome de diario

---
+++ src/js/sentimentos.js
@@ -0,0 +1,78 @@
+const textarea = document.getElementById("text");
+const countdown = document.getElementById("countdown");
+const maxLength = 250;
+
+textarea.addEventListener("input", function() {
+    const remaining = maxLength - textarea.value.length;
+
+    if (remaining >= 0) {
+        countdown.innerText = remaining;
+    } else {
+        textarea.value = textarea.value.slice(0, maxLength);
+    }
+});
+
+
+function limpar() {
+    document.getElementById("text").value = "";
+  }
+
+
+function setAlarm() {
+    if (isAlarmSet) {
+        alarmTime = "";
+        ringTone.pause();
+        content.classList.remove("disable");
+        btnSetAlarm.innerHTML = "Ativar Alarme";
+        return isAlarmSet = false;
+    }
+
+    let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+    if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
+        return alert("Insira horas e minutos válidos para ativar o alarme, por favor!");
+    }
+    alarmTime = time;
+    isAlarmSet = true;
+    content.classList.add("disable");
+    btnSetAlarm.innerHTML = "Desativar Alarme";
+}
+btnSetAlarm.addEventListener("click", setAlarm);
+
+
+const sentimentos = document.querySelector(".sentimentos");
+imgContainer.addEventListener("mousedown", function() {
+  this.classList.add("pressed");
+});
+imgContainer.addEventListener("mouseup", function() {
+  this.classList.remove("pressed");
+});
+
+function showModal() {
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function hideModal(){
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal")
+}
+
+function showModal1() {
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function hideModal1(){
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal")
+}
+
+function showModal2() {
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function hideModal2(){
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal")
+}
