786bd92842848df57e68d86125d0df8617e1993a
Nathan José França <111743490+Nathanfranca@users.noreply.github.com>
2023-10-20 11:51:36 -0300

    Update chatbot .js

--- src/js/chatbot .js
+++ src/js/chatbot .js
@@ -47,30 +47,68 @@ function appendMessage(sender, message) {
 
 
 
-const chatLog = document.getElementById('chat-log');
-const userInput = document.getElementById('user-input');
-const sendButton = document.getElementById('send-button');
-
-sendButton.addEventListener('click', sendMessage);
-
-function sendMessage() {
-    const userMessage = userInput.value;
-    if (userMessage.trim() === '') return;
+document.addEventListener('DOMContentLoaded', function() {
+    const chatLog = document.getElementById('chat-log');
+    const userInput = document.getElementById('user-input');
+    const sendButton = document.getElementById('send-button');
     
-    appendMessage('Você:', userMessage);
-    // Aqui você pode adicionar a lógica para enviar a mensagem ao chatbot e receber uma resposta da IA.
-    
-    // Simulação de resposta do chatbot (substitua isso com a lógica real):
-    setTimeout(() => {
-        const chatbotResponse = 'Olá! Bem vindo ao aplicativo web MinhaSaúde. Aqui a ideia não é substituir um psicólogo ou profissional da saúde mental, mas sim oferecer suporte emocional.Fui projetado para dar conselhos e incentivar a pessoa a falar sobre seus problemas. Você quer conversar comigo? 1 - Sim 2 - Não.';
-        appendMessage('Chatbot:', chatbotResponse);
-    }, 1000);
+    sendButton.addEventListener('click', sendMessage);
 
-    userInput.value = '';
-}
+    function sendMessage() {
+        const userMessage = userInput.value;
+        if (userMessage.trim() === '') return;
 
-function appendMessage(sender, message) {
-    const messageElement = document.createElement('div');
-    messageElement.innerHTML = `<strong>${sender}</strong>: ${message}`;
-    chatLog.appendChild(messageElement);
-}
+        appendMessage('Você:', userMessage);
+
+        if (userMessage === '1') {
+            const chatbotResponse = 'Saúde Mental é a saúde do nosso estado psicológico e emocional. É fundamental para uma vida equilibrada. Para saber mais sobre saúde mental, visite o [link] (https://exemplo.com/saudemental).';
+            appendMessage('Chatbot:', chatbotResponse);
+
+            setTimeout(() => {
+                const followupResponse = 'Gostaria de falar sobre mais algum assunto? 1 - Saúde Mental 2 - Saúde Física 3 - Saúde Emocional 4 - Ajuda.';
+                appendMessage('Chatbot:', followupResponse);
+            }, 1000);
+        } else if (userMessage === '2') {
+            const chatbotResponse = 'Saúde Física é a saúde do nosso corpo e bem-estar físico. Manter-se ativo e ter uma alimentação saudável são aspectos importantes. Para mais informações, visite [link] (https://exemplo.com/saudefisica).';
+            appendMessage('Chatbot:', chatbotResponse);
+
+            setTimeout(() => {
+                const followupResponse = 'Gostaria de falar sobre mais algum assunto? 1 - Saúde Mental 2 - Saúde Física 3 - Saúde Emocional 4 - Ajuda.';
+                appendMessage('Chatbot:', followupResponse);
+            }, 1000);
+        } else if (userMessage === '3') {
+            const chatbotResponse = 'Saúde Emocional é o cuidado com as emoções e o equilíbrio psicológico. Manter relações saudáveis e lidar com o estresse são aspectos essenciais. Saiba mais [aqui] (https://exemplo.com/saudeemocional).';
+            appendMessage('Chatbot:', chatbotResponse);
+
+            setTimeout(() => {
+                const followupResponse = 'Gostaria de falar sobre mais algum assunto? 1 - Saúde Mental 2 - Saúde Física 3 - Saúde Emocional 4 - Ajuda.';
+                appendMessage('Chatbot:', followupResponse);
+            }, 1000);
+        } else if (userMessage === '4') {
+            const chatbotResponse = 'Se você precisa de ajuda imediata, por favor, entre em contato com um profissional de saúde ou ligue para um serviço de apoio. Estamos aqui para ajudar. Para mais informações, visite [link] (https://exemplo.com/ajuda).';
+            appendMessage('Chatbot:', chatbotResponse);
+
+            setTimeout(() => {
+                const followupResponse = 'Gostaria de falar sobre mais algum assunto? 1 - Saúde Mental 2 - Saúde Física 3 - Saúde Emocional 4 - Ajuda.';
+                appendMessage('Chatbot:', followupResponse);
+            }, 1000);
+        } else {
+            const chatbotResponse = 'Seja bem-vindo ao aplicativo web MinhaSaúde. Por favor, escolha uma opção: 1 - Saúde Mental 2 - Saúde Física 3 - Saúde Emocional 4 - Ajuda.';
+            appendMessage('Chatbot:', chatbotResponse);
+        }
+
+        userInput.value = '';
+    }
+
+    function appendMessage(sender, message) {
+        const messageElement = document.createElement('div');
+        messageElement.innerHTML = `<strong>${sender}</strong>: ${message}`;
+        chatLog.appendChild(messageElement);
+    }
+
+    // Iniciar a conversa com a mensagem de boas-vindas.
+    const welcomeMessage = 'Seja bem-vindo ao aplicativo web MinhaSaúde. Por favor, escolha uma opção: 1 - Saúde Mental 2 - Saúde Física 3 - Saúde Emocional 4 - Ajuda.';
+    appendMessage('Chatbot:', welcomeMessage);
+});
+
+
